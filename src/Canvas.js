95058ccd8825e3d814521bd0c6bf6c51817c4eba
Bayleaf <152853920+bayleaf1@users.noreply.github.com>
2024-07-18 15:59:21 +0300

    fix

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,94 +1,90 @@
-import { useEffect } from "react";
+import { useEffect, useMemo, useRef } from "react";
 const CV_WIDTH = 1440;
 const CV_HEIGHT = 810;
 
 export default function Canvas({ frame }) {
   const CELL_SIZE = CV_HEIGHT / frame.height;
 
-  useEffect(() => {
-      let cv = document.querySelector("canvas");
-      const ctx = cv && cv.getContext && cv.getContext("2d");
+  const ref = useRef(null);
 
-      if (ctx) {
+  const ctx = useMemo(
+    () => (ref.current ? ref.current?.getContext?.("2d") : null),
+    [ref.current]
+  );
+
+  useEffect(() => {
+    if (ctx) {
+      clearCanvas();
+      drawNet();
+
+      function clearCanvas() {
         ctx.fillStyle = "white";
         ctx.fillRect(0, 0, CV_WIDTH, CV_HEIGHT);
-          drawNet();
-
-        function drawNet() {
-          setStyle();
-
-          for (let index = 0; index < frame.height; index++)
-            drawVerticalLine(index);
-          for (let index = 0; index < frame.width; index++)
-            drawHorizontalLine(index);
-
-          function setStyle() {
-            ctx.strokeStyle = "gray";
-            ctx.lineWidth = 0.1;
-          }
-          function drawHorizontalLine(colIndex) {
-            ctx.beginPath();
-            ctx.moveTo(colIndex * CELL_SIZE, 0);
-            ctx.lineTo(colIndex * CELL_SIZE, CV_HEIGHT);
-            ctx.stroke();
-          }
-
-          function drawVerticalLine(rowIndex) {
-            ctx.beginPath();
-            ctx.moveTo(0, rowIndex * CELL_SIZE);
-            ctx.lineTo(CV_WIDTH, rowIndex * CELL_SIZE);
-            ctx.stroke();
-          }
-        }
       }
-  }, [CELL_SIZE]);
+      function drawNet() {
+        setStyle();
 
-  useEffect(() => {
-    let cv = document.querySelector("canvas");
-    const ctx = cv && cv.getContext && cv.getContext("2d");
+        for (let index = 0; index < frame.height; index++)
+          drawVerticalLine(index);
+        for (let index = 0; index < frame.width; index++)
+          drawHorizontalLine(index);
 
-    if (ctx) {
-      drawCells(ctx);
-
-      function drawCells() {
-        for (let rowIndex = 0; rowIndex < frame.height; rowIndex++) {
-          for (let colIndex = 0; colIndex < frame.width; colIndex++) {
-            let value = frame.rows[rowIndex][colIndex];
-            if (value) drawCell(ctx, rowIndex, colIndex);
-          }
+        function setStyle() {
+          ctx.strokeStyle = "gray";
+          ctx.lineWidth = 0.1;
         }
-
-        function drawCell(ctx, rowIndex, colIndex) {
-          ctx.fillStyle = "blue";
-          ctx.fillRect(
-            colIndex * CELL_SIZE + 1,
-            rowIndex * CELL_SIZE + 1,
-            CELL_SIZE - 2,
-            CELL_SIZE - 2
+        function drawHorizontalLine(colIndex) {
+          drawLine(
+            [colIndex * CELL_SIZE, 0],
+            [colIndex * CELL_SIZE, CV_HEIGHT]
           );
         }
+
+        function drawVerticalLine(rowIndex) {
+          drawLine([0, rowIndex * CELL_SIZE], [CV_WIDTH, rowIndex * CELL_SIZE]);
+        }
+
+        function drawLine(from, to) {
+          ctx.beginPath();
+          ctx.moveTo(...from);
+          ctx.lineTo(...to);
+          ctx.stroke();
+        }
       }
+    }
+  }, [ctx]);
+
+  useEffect(() => {
+    if (ctx) {
+      fillAliveCells();
+      function fillAliveCells() {
+        drawAliceCells((...args) => drawCell(...args, "blue"));
+      }
+    }
+
+    function drawAliceCells(drawCell) {
+      for (let rowIndex = 0; rowIndex < frame.height; rowIndex++) {
+        for (let colIndex = 0; colIndex < frame.width; colIndex++) {
+          let value = frame.rows[rowIndex][colIndex];
+          if (value) drawCell(ctx, rowIndex, colIndex);
+        }
+      }
+    }
+    function drawCell(ctx, rowIndex, colIndex, color) {
+      ctx.fillStyle = color;
+      ctx.fillRect(
+        colIndex * CELL_SIZE + 1,
+        rowIndex * CELL_SIZE + 1,
+        CELL_SIZE - 2,
+        CELL_SIZE - 2
+      );
     }
 
     return () => {
       if (ctx) {
-        drawCells();
-        function drawCells() {
-          for (let rowIndex = 0; rowIndex < frame.height; rowIndex++) {
-            for (let colIndex = 0; colIndex < frame.width; colIndex++) {
-              let value = frame.rows[rowIndex][colIndex];
-              if (value) drawCell(ctx, rowIndex, colIndex);
-            }
-          }
-          function drawCell(ctx, rowIndex, colIndex) {
-            ctx.fillStyle = "white";
-            ctx.fillRect(
-              colIndex * CELL_SIZE + 1,
-              rowIndex * CELL_SIZE + 1,
-              CELL_SIZE - 2,
-              CELL_SIZE - 2
-            );
-          }
+        clearAliveCells();
+        function clearAliveCells() {
+          drawAliceCells((...args) => drawCell(...args, "white"));
         }
       }
     };
@@ -96,6 +92,7 @@ export default function Canvas({ frame }) {
   return (
     <div className="shadow-2xl bg-white rounded-xl overflow-hidden">
       <canvas
+        ref={ref}
         id="canvas"
         className="border w-full "
         width={CV_WIDTH}
